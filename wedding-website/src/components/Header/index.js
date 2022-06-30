02fdee1e0057b874b5697d298b0b532c6c1cce6c
Isabelle Bertram <isabellebertram@Isabelles-MacBook-Air.local>
2022-06-30 08:39:45 -0500

    removing background

--- wedding-website/src/components/Header/index.js
+++ wedding-website/src/components/Header/index.js
@@ -2,12 +2,30 @@ import React, { useState } from 'react';
 import Nav from '../Navigation';
 import About from '../About';
 import RSVP from '../RSVP';
+import OurStory from '../Our Story';
+import Photos from '../Photos';
+import Registry from '../Registry';
+import ThingsToDo from '../Things To Do';
+import Travel from '../Travel';
+import WeddingParty from '../Wedding Party';
 
 function Header() {
     const [currentPage, handlePageChange] = useState('About');
 
     const renderPage = () => {
         switch (currentPage) {
+            case 'Our Story':
+                return <OurStory />
+            case 'Photos':
+                return <Photos />
+            case 'Registry':
+                return <Registry />
+            case 'Things To Do':
+                return <ThingsToDo />
+            case 'Travel':
+                return <Travel />
+            case 'Wedding Party':
+                return <WeddingParty />
             case 'RSVP':
                 return <RSVP />
 
